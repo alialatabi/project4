c6db94d258b67c3b5c72fee2993a9a870fec2b15
alialatabi <ali.alatabi99.vb@gmail.com>
2022-04-26 18:27:44 +0300

    Finished the posting functionality

---
+++ network/models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    post = models.CharField(max_length=255)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} || {self.time_stamp} || {self.post}"
+
+
+class Follower(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower', default=None)
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following', default=None)
+
+    class Meta:
+        unique_together = (('follower', 'following'),)
+
+    def __str__(self):
+        return f"{self.follower} : {self.following}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment", default=None)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment", default=None)
+    comment = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.user} || {self.post} || {self.comment}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="like", default=None)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="like", default=None)
+
+    class Meta:
+        unique_together = (('post', 'user'),)
+
+    def __str__(self):
+        return f"{self.user} || {self.post}"
